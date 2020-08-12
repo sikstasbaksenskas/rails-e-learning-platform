506b39f7ea27963415acf6d3fdcff5511a57d557
sikstasbaksenskas <sikstas.baksenskas@gmail.com>
2020-08-12 23:28:06 +0300

    relationship between course & user models

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
